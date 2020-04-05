627094a282c120e87ccbbd71dbd195ed37e90067
WalterStern1 <fasterpermits@Fasters-MacBook-Pro-6.local>
2020-04-04 18:06:19 -0700

    third commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,20 +4,40 @@ import csv
 csvpath = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
-
     csvreader = csv.reader(csvfile, delimiter=',')
-    for date in csvreader:
+    
+    
+    for row in csvreader:
         print('Financial Analysis')
         print('-----------------------------')
         print("Total Months: " + str(len(list(csvreader)))) 
-        months = str(len(list(csvreader)))
+        
+       
 
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    previous = next(csvreader)
+
+    count = 1
+    total = int(previous[1])
+    averagediff = []
+    min = 0
+    max = 0
+    for row in csvreader:
+
+        count += 1
+        total = total + int(row[1])
+        previous[1] = int(row[1])
+    print('Total: ' + str(total))
 
 with open (csvpath) as csvfile:   
     csvreader = csv.reader(csvfile, delimiter=',')
-    for date in csvreader:
+    for row in csvreader:
         txt = open("analysis/report.txt","w+")
         H = ["Financial Analysis \n", "----------------------- \n"]
         txt.writelines(H)
-        txt.write(f"Total Months: {len(list(csvreader))}")
+        txt.write(f"Total Months: {len(list(csvreader))} \n")
+        txt.write(f"Total: {total} \n")
+    
   
